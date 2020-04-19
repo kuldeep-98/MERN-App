de87b6bbd07f73d03b6c8c5b89f556fcd1d26208
Kuldeep <kuldeepbhama4455@gmail.com>
2020-04-19 23:30:02 +0530

    Create Action for Profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -22,7 +22,7 @@ const Dashboard = ({ getCurrentProfile, auth:{ user }, profile: { profile, loadi
                 <Fragment>has</Fragment>
             ) : (
                 <Fragment>
-                    <p>You have not yet setu a profile, please add some info</p>
+                    <p>You have not yet setup a profile, please add some info</p>
                     <Link to='/create-profile' className='btn btn-primary my-1'>
                         Create Profile
                     </Link>
