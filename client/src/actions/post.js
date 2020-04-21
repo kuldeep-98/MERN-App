d9bc100d61564699aa38134d974cf54a2c404e4a
Kuldeep <kuldeepbhama4455@gmail.com>
2020-04-22 02:56:22 +0530

    Add Comments on Post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -6,7 +6,9 @@ import {
     POST_ERROR,
     UPDATE_LIKES,
     DELETE_POST,
-    ADD_POST
+    ADD_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT
 } from './types';
 
 // Get posts
@@ -85,7 +87,7 @@ export const addPost = formData => async dispatch =>{
         headers: {
             'Content-Type':'application/json'
         }
-    }
+    };
     try {
         const res = await axios.post('/api/posts', formData, config);
 
@@ -112,6 +114,49 @@ export const getPost = id => async dispatch =>{
             type: GET_POST,
             payload: res.data
         });
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+// Add comment
+export const addComment = (postId, formData) => async dispatch =>{
+    const config = {
+        headers: {
+            'Content-Type':'application/json'
+        }
+    };
+    try {
+        const res = await axios.post(`/api/posts/comment/${postId}`, formData, config);
+
+        dispatch({
+            type: ADD_COMMENT,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Comment Added', 'success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+// Delete comment
+export const deleteComment = (postId, commentId) => async dispatch =>{
+    try {
+        await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+
+        dispatch({
+            type: REMOVE_COMMENT,
+            payload: commentId
+        });
+
+        dispatch(setAlert('Comment Removed', 'success'));
     } catch (err) {
         dispatch({
             type: POST_ERROR,
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,6 +5,7 @@ import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
 import { getPost } from '../../actions/post';
 import { Link } from 'react-router-dom';
+import CommentForm from './CommentForm';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
     useEffect(()=>{
@@ -18,6 +19,7 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
                     Back To Posts
                 </Link>
                 <PostItem post={post} showActions={false} />
+                <CommentForm postId={post._id} />
             </Fragment>
         )
     )
