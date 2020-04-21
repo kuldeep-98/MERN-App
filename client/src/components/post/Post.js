d17527e4717b3e64cab961438fab6a336d8449dd
Kuldeep <kuldeepbhama4455@gmail.com>
2020-04-22 03:25:02 +0530

    Display and Remove Comments

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -6,11 +6,12 @@ import PostItem from '../posts/PostItem';
 import { getPost } from '../../actions/post';
 import { Link } from 'react-router-dom';
 import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
     useEffect(()=>{
         getPost(match.params.id)
-    },[getPost]);
+    },[getPost,match.params.id]);
     return (
         loading || post === null ? (<Spinner />
         ) : (
@@ -20,13 +21,19 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
                 </Link>
                 <PostItem post={post} showActions={false} />
                 <CommentForm postId={post._id} />
+                <div className="comments">
+                    {post.comments.map(comment => (
+                        <CommentItem key={comment._id} comment={comment} postId={post._id} />
+                    ))}
+                </div>
             </Fragment>
         )
-    )
-}
+    );
+};
 
 Post.propTypes = {
     getPost: PropTypes.func.isRequired,
+    post: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
