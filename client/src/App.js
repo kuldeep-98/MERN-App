fad6ba0abb4903bd764a0f6765685449ef76867b
Kuldeep <kuldeepbhama4455@gmail.com>
2020-04-22 18:09:58 +0530

    Organize Routes And Add NotFound

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,8 @@ import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
-import Alert from './components/layout/Alert';
-import Dashboard from './components/dashboard/Dashboard';
-import CreateProfile from './components/profile-form/CreateProfile';
-import EditProfile from './components/profile-form/EditProfile';
-import AddExperience from './components/profile-form/AddExperience';
-import AddEducation from './components/profile-form/AddEducation';
-import Profile from './components/profile/Profile';
-import Profiles from './components/profiles/Profiles';
-import Posts from './components/posts/Posts';
-import Post from './components/post/Post';
-import PrivateRoute from './components/routing/PrivateRoute';
+import Routes from './components/routing/Routes';
+
 //Redux
 
 import { Provider } from 'react-redux';
@@ -28,7 +17,7 @@ if (localStorage.token) {
 }
 
 const App = () => {
-  useEffect(()=>{
+  useEffect(() => {
     store.dispatch(loadUser());
   }, []);
 
@@ -37,26 +26,13 @@ const App = () => {
       <Router>
         <Fragment>
           <Navbar />
-          <Route exact path='/' component={Landing} />
-          <section className="container">
-            <Alert />
-            <Switch>
-              <Route exact path='/register' component={Register} />
-              <Route exact path='/login' component={Login} />
-              <Route exact path='/profiles' component={Profiles} />
-              <Route exact path='/profile/:id' component={Profile} />
-              <PrivateRoute exact path='/dashboard' component={Dashboard} />
-              <PrivateRoute exact path='/create-profile' component={CreateProfile} />
-              <PrivateRoute exact path='/edit-profile' component={EditProfile} />
-              <PrivateRoute exact path='/add-experience' component={AddExperience} />
-              <PrivateRoute exact path='/add-education' component={AddEducation} />
-              <PrivateRoute exact path='/posts' component={Posts} />
-              <PrivateRoute exact path='/posts/:id' component={Post} />
-            </Switch>
-          </section>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Routes component={Routes} />
+          </Switch>
         </Fragment>
       </Router>
     </Provider>
   )
 };
-  export default App;
+export default App;
